6c66cd3267a397a88258ab9c9bdd597852da1ef6
Romesha-Shahid <sweetieromesha@gmail.com>
2025-02-02 06:41:52 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,41 +1,39 @@
  "use client";
 
- import Link from "next/link";
- import React from "react";
- 
- 
- const Navbar = () =>{
-    return(
+import Link from "next/link";
+import React from "react";
 
-      <div>
+const Navbar = () => {
+  return (
+    <div className="bg-slate-300">
+      <nav className="h-16 py-2 px-4 flex flex-wrap items-center justify-between text-teal-500 font-bold">
+        {/* Logo */}
+        <h1 className="text-2xl italic">
+          <a href="#!">Romi.com</a>
+        </h1>
 
-<nav className="bg-slate-300 h-10 py-1 px-2 flex justify-between items-center text-teal-500 font-bold">
-      <div>
-        <h1 className=" text-2xl italic">
-            <a href="#!">Romi.com</a>
-            </h1> 
-      </div>
-      <div>
-         <ul className="flex space-x-7">
-         <li><Link  href="/contact">contact</Link></li>
-         <li><Link href="/about">about</Link></li>
-         <li><Link href="/"></Link>home</li>
+        {/* Navigation Links */}
+        <ul className="flex flex-wrap space-x-4 md:space-x-7 text-sm md:text-base">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link href="/about">About</Link>
+          </li>
+        </ul>
 
-         </ul>      
-        </div>
-      <div>
+        {/* Signup Link */}
         <ul>
-          <li><Link href="/signup">signup</Link></li>
-          </ul>
-           </div>
-           
-
-    </nav>
+          <li>
+            <Link href="/signup">Signup</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
+  );
+};
 
-      
-
-    );
-}
- export default  Navbar
- 
+export default Navbar;
